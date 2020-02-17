3049e29bf8c9c122c1a7dc2acb4a013a78059c9a
Rubén Dario Betancur Rairán <rbetancurrairan@gmail.com>
2020-02-16 23:23:25 -0500

    Imports the shared module

--- w-bank/apps/continental-region/src/app/app.module.ts
+++ w-bank/apps/continental-region/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { UiModule } from '@w-bank/ui';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
